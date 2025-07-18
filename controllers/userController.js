2ceb9f9532f3cd6cb0893b44559d0acad3a3a471
kai <kerul2108@gmail.com>
2025-07-18 14:44:27 +0700

    apus validasi

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,16 +3,6 @@ const userQueries = require('../query/userQuery');
 exports.createUser = async (req, res) => {
   try {
     const { name, email, password } = req.body;
-
-    if (!name) return res.status(400).json({ error: 'name is required' });
-    if (!email) return res.status(400).json({ error: 'email is required' });
-    if (!password) return res.status(400).json({ error: 'password is required' });
-
-    const existingUser = await userQueries.cekExistingUser(name);
-    if (existingUser.length > 0) {
-      return res.status(409).json({ error: 'Name already exists' });
-    }
-
     const newUser = await userQueries.addUser(name, email, password);
     res.status(201).json(newUser);
   } catch (err) {
--- query/userQuery.js
+++ query/userQuery.js
@@ -4,15 +4,11 @@ exports.getAllUsers = async () => {
   const result = await pool.query('SELECT id, name, email FROM users ORDER BY id');
   return result.rows;
 };
+
 exports.addUser = async (name, email, password) => {
   const result = await pool.query
   ('INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *',
   [name, email, password]);
   return result.rows[0];
-};
-
-exports.cekExistingUser = async (name) => {
-  const result = await pool.query('SELECT * FROM users WHERE name = $1', [name]);
-  return result.rows;
 };
 
